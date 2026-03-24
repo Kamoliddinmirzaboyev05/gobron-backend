6e09e8b724af37dda018cf6093eab8fc1c83450d
Kamoliddin Mirzaboyev <kamoliddinmirzaboyev2005@gmail.com>
2026-03-24 10:05:20 +0500

    fix: prisma config and dependencies

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,6 @@
 import "dotenv/config"; 
- import { defineConfig } from "prisma/config"; 
+ import { defineConfig } from "@prisma/config"; 
  
  export default defineConfig({ 
    schema: "prisma/schema.prisma", 
-   migrate: { 
-     migrationsDir: "prisma/migrations", 
-   }, 
  }); 
